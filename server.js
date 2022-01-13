13c519efdbf1e55dfcdc96ae7d458a284ee4085c
asyrul21 <asyrulhafetzy.21@gmail.com>
2022-01-13 18:06:50 +0800

    remove database component

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const morgan = require("morgan");
-const { initializeDatabase } = require("./config/database");
 const http = require("http");
 const events = require("events");
 const EM = new events.EventEmitter();
@@ -23,12 +22,10 @@ app.use(express.json());
 const EmailRoutes = require("./routes/emailRoutes");
 
 app.get("/api/", (req, res) => {
+  console.log(req.body);
   res.send("Xendit Notification API is running");
 });
 app.use("/api/email", EmailRoutes);
-
-// initialise User Roles
-initializeDatabase(process.env.NODE_ENV, EM);
 
 // error middlewares
 app.use(notFound);
@@ -36,19 +33,6 @@ app.use(errorHandler);
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
-module.exports =
-  process.env.NODE_ENV === "test"
-    ? server.listen(
-        PORT,
-        console.log(
-          `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-        )
-      )
-    : // wait for the database is loaded before starting listening
-      EM.on("initializationDone", () => {
-        server.listen(PORT, () => {
-          console.log(
-            `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-          );
-        });
-      });
+module.exports = server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
